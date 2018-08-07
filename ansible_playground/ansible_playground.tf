17b30fa26e4126650667c5c7e19fd913dd09eb04
delali <delali.kwami@gmail.com>
2018-08-06 22:27:52 -0400

    Adding directives to create security groups and network rules

--- ansible_playground/ansible_playground.tf
+++ ansible_playground/ansible_playground.tf
@@ -4,18 +4,61 @@ provider "aws" {
   region     = "${var.AWS_REGION}"
 }
 
-resource "aws_instance" "ansible_controller" {
-  ami           = "ami-28e07e50"
-  instance_type = "t2.micro"
+# SUPPLY AWS KEY PAIR FOR LOGGING IN
+#resource "aws_key_pair" "winlogin" {
+#  key_name	= ""
+#  public_key	= ""
+#}
+
+# CREATE SECURITY GROUP FOR EC2 INSTANCE
+# CREATE NETWORK ACL RULES TO ALLOW INBOUND RDP AND WINRM CONNECTIONS
+resource "aws_security_group" "windows_sg" {
+  name = "${var.WINDOWS_SECURITY_GROUP}"
+
+  ingress {
+    from_port 	= 0
+    to_port 	= 3389
+    protocol 	= "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "inbound rule for RDP connections"
+  }
+
+  ingress {
+    from_port 	= 0
+    to_port 	= 5986
+    protocol 	= "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "inbound rule for WinRM https connections"
+  }
+
+  ingress {
+    from_port 	= 0
+    to_port 	= 5985
+    protocol 	= "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "inbound rule for WinRM connections"
+  }
 }
+
+
+
+
+# RESOURCE CREATION
+
+#resource "aws_instance" "ansible_controller" {
+#  ami           = "ami-28e07e50"
+#  instance_type = "t2.micro"
+#}
 
 resource "aws_instance" "web1" {
-  ami		= "ami-6d336015"
-  instance_type = "t2.micro"
+  ami			= "ami-6d336015"
+  instance_type 	= "t2.micro"
+  key_name		= "${var.AWS_KEY_PAIR}"
+  security_groups	= ["${var.WINDOWS_SECURITY_GROUP}"]
 }
 
-resource "aws_instance" "web2" {
-  ami		= "ami-6d336015"
-  instance_type = "t2.micro"
-}
+#resource "aws_instance" "web2" {
+#  ami		= "ami-6d336015"
+#  instance_type = "t2.micro"
+#}
 
--- ansible_playground/vars.tf
+++ ansible_playground/vars.tf
@@ -1,5 +1,8 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
+variable "AWS_KEY_PAIR" {}
+variable "WINDOWS_SECURITY_GROUP" {}
+
 variable "AWS_REGION" {
   default = "us-west-2"
 }
